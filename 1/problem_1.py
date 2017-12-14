6f1c40ef758a685526ce6192f1e69e7dd9b92914
EduardEdiJerkovic <eduard.edi.jerkovic@gmail.com>
2017-12-14 13:03:16 +0100

    Second part of problem1 completed.

--- 1/problem_1.py
+++ 1/problem_1.py
@@ -4,15 +4,21 @@ def problem_1():
     tmp, *tail = numbers
 
     if numbers[-1] == numbers[0]:
-        result = int(numbers[-1])
+        result1 = int(numbers[-1])
     else:
-        result = 0
+        result1 = 0
 
     for num in tail:
         if tmp == num:
-            result += int(tmp)
+            result1 += int(tmp)
         tmp = num
+
+    result2= 0
+    for i in range(len(numbers) // 2):
+        if numbers[i] == numbers[i + len(numbers) // 2]:
+            result2 += 2 * int(numbers[i])
+
     
-    return result
+    return result1, result2
 
 print(problem_1())
